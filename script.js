56929e719425f4a530c1ff3a3dace8db7457b009
Matheus Ferreira <matheus.fer33@gmail.com>
2022-05-03 00:10:21 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const aluguel = document.querySelector('.aluguel');
+const emprestar = document.querySelector('.emprestar');
+
+aluguel.addEventListener('click', () => {
+   console.log('clicou no aluguel')
+})
+
+emprestar.addEventListener('click', () => {
+   console.log('clicou no emprestar')
+})
